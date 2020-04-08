33dc085c2e4c7c378fdc46407fc5d99d6b6f5d14
18MJ043 <machuringo0904@gmail.com>
2020-04-08 23:35:08 +0900

    add new file

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <!-- jQuery -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <!-- font-awesome -->
+    <script src="https://kit.fontawesome.com/c81fa55527.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <div class="header-bottom">
+                <i id="open"class="fas fa-bars"></i>
+            <div class="pull-down">
+                <div class="pull-top">
+                    <a href="">Blog</a>
+                    <a class="p2" href="">Profile</a>
+                </div>
+                <div class="pull-bottom">
+                    <a href=""><i class="fab fa-twitter-square"></i></a>
+                    <a href=""><i class="fab fa-instagram-square"></i></a>
+                    <a class="p2" href=""><i class="fab fa-github-square"></i></a>
+                    <a class="p2" href=""><i class="fas fa-envelope-square"></i></a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <script src="js/script.js"></script>
+</body>
+</html>
